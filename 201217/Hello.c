98e5e5344c8874e2ea13e0c334f89ef64bd06b9e
DESKTOP-SQA3D42\jh <tla149@daum.net>
2020-12-07 18:16:02 +0900

    201207_열혈강의C프로그래밍_시작

---
+++ 201217/Hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// 문제 02-1 문제 1
+// int main(void){
+//     printf("홍길동\n홍 길 동\n홍  길  동\n");
+//     return 
+//     0;
+// }
+
+// 문제 02-1 문제 2
+// int main(void){
+//     printf("이름 : 심재형\n");
+//     printf("주소 : 지산동89번길 11\n");
+//     printf("번호 : 01033636744\n");
+
+//     return 0;
+// }
